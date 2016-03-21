10a1036dcbc40d379d8f4d8a0ed43365989bca0e
Eric Carlson <erccarls@ucsc.edu>
2016-03-20 18:32:14 -0700

    update setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = "GammaLike",
+    packages = ["numpy","scipy","h5py","pyfits","iminuit","healpy","cPickle","astropy"],
+    version = "1.0.0",
+    description = "Gmma-ray Template Regression Toolkit",
+    author = "Eric Carlson",
+    author_email = "eric.carlson.128@gmail.com",
+    url = "http://planck.ucsc.edu/GammaLike/",
+    download_url = "http://chardet.feedparser.org/download/python3-chardet-1.0.1.tgz",
+    keywords = ["Gamma ray","dark matter","Fermi"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Data Analysis",
+        ]
+)
